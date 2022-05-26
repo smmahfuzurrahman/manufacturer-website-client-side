702166985bf698e8ac04f677082d82cc16c0f34d
Mahfuzur Rahman <smmahfuz787@gmail.com>
2022-05-26 23:40:55 +0600

    Update Profile

--- src/Components/Home/Message.js
+++ src/Components/Home/Message.js
@@ -4,7 +4,7 @@ const Message = () => {
     return (
         <div class="hero w-full">
             <div class="hero-content flex-col lg:flex-row">
-                <img className='w-full lg:w-96' src="https://media.istockphoto.com/vectors/anniversary-celebration-gold-icon-vector-id637019928?b=1&k=20&m=637019928&s=612x612&w=0&h=CFsmC9FqR5v9z3CBi8xEQ_4mJV83xVGGDAtMNBNMiuA=" class="max-w-sm rounded-lg shadow-2xl" alt='' />
+                <img className='sm:w-50 lg:w-96' src="https://media.istockphoto.com/vectors/anniversary-celebration-gold-icon-vector-id637019928?b=1&k=20&m=637019928&s=612x612&w=0&h=CFsmC9FqR5v9z3CBi8xEQ_4mJV83xVGGDAtMNBNMiuA=" class="max-w-sm rounded-lg shadow-2xl" alt='' />
                 <div className='text-left lg:mx-10'>
                     <h1 class="text-5xl font-bold">50 Year Celebration</h1>
                     <p class="py-6">Wishing someone a happy work anniversary is so relevant and meaningful. It’s one of the most fundamental forms of employee appreciation and employee recognition.</p>
--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -3,62 +3,24 @@ import './UserProfile.css';
 import img from '../../images/about/10861 A.JPG';
 const UserProfile = () => {
     return (
-        <div className="portfolio-div mt-5">
-            <h1 className="text-2xl underline mb-10">My portfolio</h1>
-            <div className="about_me_content">
-                <div className="my_img">
-                    <img src={img} alt="" />
-                </div>
-
-                <div className="about_me_info">
-                    <div className="educationa_info">
-                        <p>
-                            Name: <span>S.M. Mahfuzur Rahman</span>
-                        </p>
-                        <p>
-                            Email: <span>smmahfuz787@gmail.com</span>
-                        </p>
-                        <p>
-                            Mobile: <span>01629504411</span>
-                        </p>
-                        <p>
-                            Post Graduation:
-                            <span>
-                                Southern University Bangladesh (2022)
-                            </span>
-                        </p>
-                    </div>
-
-                    <div className="skills">
-                        <h3>Programming Skills</h3>
-                        <p>
-                            I started my web development journey from the beginning of 2022
-                            and learned the following skills:
-                        </p>
-                        <ul>
-                            <li>html</li>
-                            <li>css</li>
-                            <li>tailwind css</li>
-                            <li>bootstrap</li>
-                            <li>javascript</li>
-                            <li>react js</li>
-                            <li>node js</li>
-                            <li>mongodb</li>
-                            <li>firebase</li>
-                            <li>git</li>
-                        </ul>
-                    </div>
-
-                    <div>
-                        <h3>My Project Link</h3>
-                        <ul>
-                            <li className="mb-3"><a href="story-keeper-371db.web.app">Project 1</a></li>
-                            <li className="mb-3"><a href="jack-stick.web.app">Project 2</a></li>
-                            <li className="mb-3"><a href="https://micro-dot.github.io/Golden-Convention-Center/">Project 3</a></li>
-                        </ul>
-                    </div>
+        <div class="hero ">
+            <div class="hero-content flex-col lg:flex-row">
+                <img className="w-40" src={img} alt='' />
+                <div>
+                    <h1 class="text-5xl font-bold mb-16">My Profile</h1>
+                    <p class="py-1 text-xl">Name: S.M. Mahfuzur Rahman <span></span></p>
+                    <p class="py-1 text-xl">Email: smmahfuz787@gmail.com<span></span></p>
+                    <p class="py-1 text-xl">Phone Number: 01629504411 <span></span></p>
+                    <p class="py-1 text-xl">Address: Boropol Road, China House, Chittagong<span></span></p>
+                    <h1 class="text-2xl font-bold mt-16">Some of Project</h1>
+                    <ul className="grid gap-2">
+                        <li><a href="story-keeper-371db.web.app">Project 1</a></li>
+                        <li><a href="jack-stick.web.app">Project 2</a></li>
+                        <li><a href="https://micro-dot.github.io/Golden-Convention-Center/">Project 3</a></li>
+                    </ul>
                 </div>
             </div>
+
         </div>
     );
 };
